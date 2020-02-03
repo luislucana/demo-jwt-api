4e09293d00c994b74f5515c0dd261f306551e10a
Luis Lucana <luislucana@gmail.com>
2020-02-03 01:12:37 -0300

    Versão inicial do projeto demo-jwt-api.

---
+++ src/main/java/br/com/jwt/web/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.jwt.web.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Consultar
+ * https://dzone.com/articles/spring-boot-security-json-web-tokenjwt-hello-world
+ */
+@RestController
+public class HelloController {
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello World";
+    }
+}
